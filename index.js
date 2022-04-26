d0738330e3e9650018ab4191667195abc4930e93
johncalabro <j.calabro4@gmail.com>
2022-04-26 13:59:53 -0400

    commit tree 82b97191f45828000e82bc1ff3e9deac70da3e6c

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function() {
+    return ['Antonia', 'Nuru']
+}
+
+const returnLastTwoDrivers = function() {
+    return ['Amari', 'Mo']
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(x) {
+    return function (x) {
+        return x * x
+    }
+}
+
+const fareDoubler = multiply (2)
+    
+
+
+
